e9046ef7f84c3aafe255eb67e2b1be579c309c76
Miriam Lang <miriam.lang@student.reutlingen-university.de>
2020-07-09 21:40:34 +0200

    import deleted

--- app/src/main/java/com/example/flyingfish/gameObjects/background/Background.java
+++ app/src/main/java/com/example/flyingfish/gameObjects/background/Background.java
@@ -1,11 +1,9 @@
 package com.example.flyingfish.gameObjects.background;
 
 import android.graphics.drawable.Drawable;
-import android.util.Log;
 import android.view.ViewGroup;
 import android.widget.FrameLayout;
 import android.widget.ImageView;
-
 import com.example.flyingfish.Constants;
 import com.example.flyingfish.gameObjects.GameObject;
 
@@ -36,26 +34,26 @@ public class Background extends GameObject {
     @Override
     public void draw() {
 
-        float widthRatio = (float) Constants.SCREEN_WIDTH/Constants.GOAL_WIDTH_RATIO;
-        float heightRatio = (float) Constants.SCREEN_HEIGHT/Constants.GOAL_HEIGHT_RATIO;
+        float widthRatio = (float) Constants.SCREEN_WIDTH / Constants.GOAL_WIDTH_RATIO;
+        float heightRatio = (float) Constants.SCREEN_HEIGHT / Constants.GOAL_HEIGHT_RATIO;
 
-        int width = (int) (this.graphic.getIntrinsicWidth()*widthRatio);
-        int height = (int) (this.graphic.getIntrinsicHeight()*heightRatio);
+        int width = (int) (this.graphic.getIntrinsicWidth() * widthRatio);
+        int height = (int) (this.graphic.getIntrinsicHeight() * heightRatio);
 
 
         FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) this.imageView.getLayoutParams();
         params.width = width;
         params.height = height;
         params.leftMargin = (int) this.x;
-        params.topMargin = (int) (this.y-height);
+        params.topMargin = (int) (this.y - height);
         this.imageView.setLayoutParams(params);
 
 
         FrameLayout.LayoutParams params2 = (FrameLayout.LayoutParams) this.imageView2.getLayoutParams();
         params2.width = width;
         params2.height = height;
-        params2.leftMargin = (int )this.x2;
-        params2.topMargin = (int) (this.y-height);
+        params2.leftMargin = (int) this.x2;
+        params2.topMargin = (int) (this.y - height);
         this.imageView2.setLayoutParams(params2);
 
     }
@@ -65,15 +63,15 @@ public class Background extends GameObject {
         this.x -= speed;
         this.x2 -= speed;
 
-        float widthRatio = (float) Constants.SCREEN_WIDTH/Constants.GOAL_WIDTH_RATIO;
-        int width = (int) (this.graphic.getIntrinsicWidth()*widthRatio);
+        float widthRatio = (float) Constants.SCREEN_WIDTH / Constants.GOAL_WIDTH_RATIO;
+        int width = (int) (this.graphic.getIntrinsicWidth() * widthRatio);
 
-     //   if(this.x+this.graphic.getIntrinsicWidth()-20 < 0) {
-        if(x + width < 0) {
+        //   if(this.x+this.graphic.getIntrinsicWidth()-20 < 0) {
+        if (x + width < 0) {
             x = Constants.SCREEN_WIDTH - 1;     // 1 pixel overlap because of rounding errors
         }
-     //   if(this.x2+this.graphic.getIntrinsicWidth()-20 < 0) {
-        if(x2 + width < 0) {
+        //   if(this.x2+this.graphic.getIntrinsicWidth()-20 < 0) {
+        if (x2 + width < 0) {
             x2 = Constants.SCREEN_WIDTH - 1;
         }
     }
